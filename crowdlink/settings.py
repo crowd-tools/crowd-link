5ddfaac86eeded6879269f71adc00a33fd7e5b4d
Jakub Boukal <www.bagr@gmail.com>
2020-05-17 12:55:12 +0200

    :bento: Fix static & assets folder

--- crowdlink/settings.py
+++ crowdlink/settings.py
@@ -154,8 +154,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'site', 'assets')
+MEDIA_ROOT = 'site/assets/'
+
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = 'site/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
